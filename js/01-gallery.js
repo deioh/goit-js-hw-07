f4014a9607fe753be194fd6e58b6082458f76064
deioh <143625578+deioh@users.noreply.github.com>
2024-05-11 18:59:06 +0800

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+// Gallery items
+const gallery = document.querySelector(".gallery");
+
+// Loop through gallery items and create a list item for each
+galleryItems.forEach((item) => {
+  let li = `    
+    <li class="gallery__item">
+    <!-- Gallery item -->
+    <a class="gallery__link" href="javascript:void(0);">
+      <!-- Link to open the image in lightbox -->
+      <img
+        class="gallery__image"
+        src="${item.preview}"
+        data-source="${item.original}"
+        alt="${item.description}"
+      />
+      <!-- Image for the gallery -->
+    </a>
+    <!-- Link container -->
+  </li>
+    `;
+  gallery.innerHTML += li;
+});
+
+// Event delegation to detect clicks on the image
+gallery.onclick = (event) => {
+  //console.log("hello");
+
+  // If the target is an image
+  if (event.target.tagName === "IMG") {
+    // Get the original image source
+    let origImage = event.target.getAttribute("data-source");
+
+    // Create a new instance of basicLightbox
+    const instance = basicLightbox.create(`
+    <img src="${origImage}" width="800" height="600">
+    `);
+    // Show the image in the lightbox
+    instance.show();
+  }
+};
