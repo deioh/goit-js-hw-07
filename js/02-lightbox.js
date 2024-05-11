b030f5b189aec0a28e20e84145bab1556b00b26b
deioh <143625578+deioh@users.noreply.github.com>
2024-05-11 22:36:31 +0800

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,60 +1,33 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
-
 // ul Gallery
 const gallery = document.querySelector(".gallery");
 
-// Loop through gallery items and create a list item for each
+// galleryItems array of objects with preview, original and description properties
+function createGalleryItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+<a class="gallery__link" href="${original}">
+  <!-- ${description} -->
+  <img
+    class="gallery__image"
+    src="${preview}"    
+    alt="${description}"
+  />`;
+    })
+    .join("");
+}
 
-galleryItems.forEach((item) => {
-  /*  
-    let li = `    
-    <li class="gallery__item">
-    <!-- Gallery item -->
-    <a class="gallery__link" href="javascript:void(0);">
-      <!-- Link to open the image in lightbox -->
-      <img
-        class="gallery__image"
-        src="${item.preview}"
-        data-source="${item.original}"
-        alt="${item.description}"
-      />
-      <!-- Image for the gallery -->
-    </a>
-    <!-- Link container -->
-  </li>
-    `;
-*/
-  let li = `
-      <li class="gallery__item">
-        <a class="gallery__link" href="javascript:void(0)">
-          <img
-            class="gallery__image"
-            src="${item.preview}"        
-            alt="${item.description}"
-          />
-        </a>
-      </li>
-      `;
-  gallery.innerHTML += li;
-});
+// Create gallery items markup
+const markup = createGalleryItem(galleryItems);
 
-// Event delegation to detect clicks on the image
-gallery.onclick = (event) => {
-  //console.log("hello");
+// Insert gallery items markup into the page
+gallery.insertAdjacentHTML("beforeend", markup);
 
-  // If the target is an image
-  if (event.target.tagName === "IMG") {
-    // Get the original image source
-    let origImage = event.target.getAttribute("data-source");
+// Create a SimpleLightbox instance
+let galleryHandler = new SimpleLightbox(".gallery a");
 
-    // Create a new instance of basicLightbox
-    const instance = basicLightbox.create(`
-    <img src="${origImage}" width="800" height="600">
-    `);
-    // Show the image in the lightbox
-    instance.show();
-  }
-};
+// Event handler for the SimpleLightbox "show.simplelightbox" event
+galleryHandler.on("show.simplelightbox");
